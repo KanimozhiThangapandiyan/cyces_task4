02c2a24052d789298f9d176b9e1d7bcbe643e844
KanimozhiThangapandiyan <mtkanimozhi@gmail.com>
2024-04-23 20:48:05 +0530

    changes

--- apps/common/serializers/eduandcert_serializers.py
+++ apps/common/serializers/eduandcert_serializers.py
@@ -4,6 +4,7 @@ from .degree_serializers import DegreeSerializer
 from .certification_serializer import CertificationSerializer
 
 class EducationAndCertificationsSerializer(serializers.ModelSerializer):
+    certifications=CertificationSerializer()
     class Meta:
         model = EducationAndCertifications
         fields = ['degree','year_of_passing','school','certifications']
--- apps/webportal/views.py
+++ apps/webportal/views.py
@@ -19,40 +19,65 @@ class PersonalDetailsViewSet(viewsets.ViewSet):
     serializer_class = PersonalDetailsSerializer
 
     @action(detail=False, methods=['post'])
-    def add_user_register_details(self, request):
-        personal_serializer = PersonalDetailsSerializer(data=request.data.get('personal_details'))
-        educational_serializer = EducationAndCertificationsSerializer(data=request.data.get('education_and_certifications'))
-        work_serializer = WorkDetailsSerializer(data=request.data.get('work_details'))
-        employment_history_serializer = EmploymentHistorySerializer(data=request.data.get('employment_history'))
-        awards_serializer = AwardsSerializer(data=request.data.get('awards'))
-        preference_serializer = PreferencesSerializer(data=request.data.get('preferences'))
+    def add_user_register_details(self, request, pk=None):
+        # print(request.data['data'])
+        # breakpoint()
+        personal_serializer = PersonalDetailsSerializer(data=request.data['personal_details'])
+        educational_serializer = EducationAndCertificationsSerializer(data=request.data['education_and_certifications'])
+        #certificate_serializer = CertificationSerializer(data=request.data['certificate_details'])
+        work_serializer = WorkDetailsSerializer(data=request.data['work_details'])
+        employee_history_serializer = EmploymentHistorySerializer(data=request.data['employment_history'])
+        awards_serializer = AwardsSerializer(data=request.data['awards'])
+        preference_serializer = PreferencesSerializer(data=request.data['preferences'])
 
-        # Validate all serializers
-        validation_errors = {}
+        if personal_serializer.is_valid() and educational_serializer.is_valid() and work_serializer.is_valid() and employee_history_serializer.is_valid() and awards_serializer.is_valid() and preference_serializer.is_valid():
+            personal_serializer.save()
+            educational_serializer.save()
+            #certificate_serializer.save()
+            work_serializer.save()
+            employee_history_serializer.save()
+            awards_serializer.save()
+            preference_serializer.save()
 
-        serializers_data = {
-            'personal_details': personal_serializer,
-            'educational_details': educational_serializer,
-            'work_details': work_serializer,
-            'employment_history': employment_history_serializer,
-            'awards_details': awards_serializer,
-            'preference_details': preference_serializer,
-        }
+            return Response({"status": "success", 'personal_details':personal_serializer.data, 'educational_details':educational_serializer.data, 'certificate_details':certificate_serializer.data, 'work_details':work_serializer.data, 'employee_history_details':employee_history_serializer.data, 'awards_details':awards_serializer.data, 'preference_details':preference_serializer.data})
+        # return Response({'status':'error', 'personal_details':personal_serializer.errors, 'educational_details':educational_serializer.errors, 'certificate_details':certificate_serializer.errors})
+        return Response({'status':'error', 'personal_details':personal_serializer.errors})
+    
+   
+    # def add_user_register_details(self, request):
+    #     personal_serializer = PersonalDetailsSerializer(data=request.data.get('personal_details'))
+    #     educational_serializer = EducationAndCertificationsSerializer(data=request.data.get('education_and_certifications'))
+    #     work_serializer = WorkDetailsSerializer(data=request.data.get('work_details'))
+    #     employment_history_serializer = EmploymentHistorySerializer(data=request.data.get('employment_history'))
+    #     awards_serializer = AwardsSerializer(data=request.data.get('awards'))
+    #     preference_serializer = PreferencesSerializer(data=request.data.get('preferences'))
 
-        for serializer_name, serializer in serializers_data.items():
-            if not serializer.is_valid():
-                validation_errors[serializer_name] = serializer.errors
+    #     # Validate all serializers
+    #     validation_errors = {}
+
+    #     serializers_data = {
+    #         'personal_details': personal_serializer,
+    #         'educational_details': educational_serializer,
+    #         'work_details': work_serializer,
+    #         'employment_history': employment_history_serializer,
+    #         'awards_details': awards_serializer,
+    #         'preference_details': preference_serializer,
+    #     }
+
+    #     for serializer_name, serializer in serializers_data.items():
+    #         if not serializer.is_valid():
+    #             validation_errors[serializer_name] = serializer.errors
         
-        if validation_errors:
-            return Response({'errors': validation_errors}, status=status.HTTP_400_BAD_REQUEST)
+    #     if validation_errors:
+    #         return Response({'errors': validation_errors}, status=status.HTTP_400_BAD_REQUEST)
 
-        # Save data using all serializers
-        personal_instance = personal_serializer.save()
-        educational_instance = educational_serializer.save()
-        work_instance = work_serializer.save()
-        employment_history_instance = employment_history_serializer.save()
-        awards_instance = awards_serializer.save()
-        preference_instance = preference_serializer.save()
+    #     # Save data using all serializers
+    #     personal_instance = personal_serializer.save()
+    #     educational_instance = educational_serializer.save()
+    #     work_instance = work_serializer.save()
+    #     employment_history_instance = employment_history_serializer.save()
+    #     awards_instance = awards_serializer.save()
+    #     preference_instance = preference_serializer.save()
 
-        # Return success response
-        return Response({'message': 'Data saved successfully'}, status=status.HTTP_201_CREATED)
+    #     # Return success response
+    #     return Response({'message': 'Data saved successfully'}, status=status.HTTP_201_CREATED)
